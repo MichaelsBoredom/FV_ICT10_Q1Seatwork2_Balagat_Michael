6b6a0a90c08cfa888709cb3319dc8399ee325d63
MichaelsBoredom <tryfrnz1@gmail.com>
2025-08-30 20:00:20 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from pyscript import display
+
+display("Hello, Grade 10")
+print("Hello, 10 Sapphire!")
+
+x = "False"
+display(type(x))
+
+# This is a comment
+
+a = 25 
+b = 3.5
+c = True 
+
+print = '5'
+display(print)
