6ba7f10f41de18eb2e53ddafc926546da918516b
MichaelsBoredom <tryfrnz1@gmail.com>
2025-08-30 20:03:41 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+from pyscript import display
+
+restaurant_name = "Café Lumière" # str
+owner_name = "Michael Balagat" # str
+year_established = 2014 # int
+popular_item_price = 185.00 # float
+has_delivery = True # bool
+product_names = ["Coffee Frappuccino", "Iced Macchiato", "Iced Latte"] # 1D array, string
+business_hours = {
+    "open": "9:00",
+    "close": "11:00"
+} # array
+menu_prices = [
+    {
+        "name": "Coffee Frappuccino",
+        "price": 185.00
+    },
+    {
+        "name": "Iced Macchiato",
+        "price": 142.00
+    },
+    {
+        "name": "Iced Latte",
+        "price": 150.00
+    },
+    {
+        "name": "Caffe Mocha",
+        "price": 125.00
+    },
+    {
+        "name": "Caffe Americano",
+        "price": 160.00
+    },
+    {
+        "name": "Banana Bread",
+        "price": 105.00
+    },
+    {
+        "name": "Glazed Doughnut",
+        "price": 95.00
+    },
+    {
+        "name": "Susage Roll",
+        "price": 175.00
+    },
+    {
+        "name": "French Butter Croissant",
+        "price": 145.00
+    },
+] # array
+common_allergens = ["dairy", "wheat", "soy", "peanuts", "tree", "nuts", "fish", "shellfish"] # 1D array, string
+tax_rate = 0.08 # float
